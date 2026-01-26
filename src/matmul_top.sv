3323890c387ba9cb88955f033b06c3c91ebc8bcc
GClishe <clishegavin@gmail.com>
2026-01-26 10:55:34 -0500

    skeleton for top.sv

---
+++ src/matmul_top.sv
@@ -0,0 +1,18 @@
+`timescale 1ns/1ps
+
+/*
+ For this project, I will first store matrices written in A.mem and B.mem (potentially more later) to FPGA ROM.
+ Then, upon a button press on the FPGA, matrices will be read and fed into 
+*/
+module matmul_top.sv(
+
+
+
+);
+
+
+
+
+
+
+endmodule 
